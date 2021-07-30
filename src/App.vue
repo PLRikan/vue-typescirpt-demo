433b16837a039ab099e100cfda7894565be265fe
phamlap <internit01@okxe.vn>
2021-07-30 13:09:09 +0700

    demo vue typescript

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">order by title</button>
+        <button @click="handleClick('salary')">order by salary</button>
+        <button @click="handleClick('location')">order by location</button>
+      </div>
+    </header>
+    <job-list :jobs="jobs" :order="order" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import JobList from './components/jobsList.vue'
+import Job from './types/Job'
+import OrderTerm from './types/OrderTerm'
+
+
+export default defineComponent({
+  name: 'App',
+  components: { JobList },
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: 'IT', location: 'lon lon ranch', salary: 30000, id:'1' },
+      { title: 'farm worker', location: 'lon lon ranch', salary: 40000, id: '2' },
+      { title: 'MK', location: 'lon lon ranch', salary: 50000, id: '3' },
+      { title: 'Design', location: 'lon lon ranch', salary: 60000, id: '4' },
+    ])
+
+    const order = ref<OrderTerm>('title')
+
+    const handleClick = (term: OrderTerm):void => {
+      order.value = term
+    }
+
+    return { jobs, handleClick, order }
+  },
+});
+</script>
+
+<style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background:  #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
